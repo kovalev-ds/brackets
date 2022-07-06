9fec5923b6afae96ab6c36353ebc185aa09a21da
kovvdn <40830091+kovvdn@users.noreply.github.com>
2022-07-06 23:20:44 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  return (
+    [...str].reduce(
+      (stack, v) =>
+        bracketsConfig.some(
+          ([left, right]) => stack[stack.length - 1] === left && v === right
+        )
+          ? (stack.pop(), stack)
+          : (stack.push(v), stack),
+      []
+    ).length === 0
+  );
+};
